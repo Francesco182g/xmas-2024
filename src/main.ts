560836512fb326498c305e4f085f1d8561a1893d
francesco182g <francescogarofalo182@live.com>
2024-12-01 14:08:53 +0100

    update .gitignore to exclude main.ts and clean up main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import { AppModule } from './app/app.module';
 
 (window as Record<string, any>)['CESIUM_BASE_URL'] = '/assets/cesium/';
 
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
