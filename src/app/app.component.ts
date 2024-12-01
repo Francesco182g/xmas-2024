801c0ddcb8cf9074087bb52450b220f1f840315e
Francesco Garofalo <francescogarofalo182@live.com>
2024-12-01 13:58:24 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core'; 
 import * as Cesium from 'cesium';
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MjJmYzczZi1kNDk1LTQwMzctODJjOS0yMDg4YjMxMDg4NmEiLCJpZCI6MTgwOTQ3LCJpYXQiOjE3MzMwNTQ4NjJ9.nyj9y_0hhi_WYmRRz7S6UMs9_IzUkjyZPFEGKyUGJd4';
 
 @Component({
   selector: 'app-root',
